74e606b54d752951f5417a8e36cb00bcbb0f30e6
Marcelo Ramos <march.reymus@gmail.com>
2020-05-17 16:44:29 -0500

    clean up server code

--- server/app.js
+++ server/app.js
@@ -1,18 +1,5 @@
 
-const http = require('http');
 
-http.createServer((req, resp) => {
-    resp.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
-    let salida = {
-        nombre: 'paco',
-        edad: '89',
-        url: req.url,
-        a: 187368172
-    };
-    resp.write(JSON.stringify(salida, null, 4));
-    resp.end();
-}).listen(8080);
 
-console.log('Hello World!');
+
+
--- server/server.js
+++ server/server.js
@@ -2,16 +2,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/public', {
-  setHeaders: function(res, path) {
-    console.log(path);
-  }
-}));
-/*
-app.use(function(req, res, next) {
-  console.log(arguments);
-});
-*/
-
+app.use(express.static(__dirname + '/public'));
 
 app.listen(8080);
